63fea6c0193a126b95d891c3995319411b7b3325
Jake Siddall <jake.siddall@gmail.com>
2021-08-06 09:50:18 -0300

    Added delete functionality to posts

--- backend/app.js
+++ backend/app.js
@@ -38,14 +38,16 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added successfully!'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully!',
+      postId: createdPost._id
+    });
   });
 });
 
 
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
   Post.find()
     .then(documents => {
       res.status(200).json({
@@ -54,5 +56,12 @@ app.use('/api/posts', (req, res, next) => {
       });
     });
 });
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: 'Post deleted!' });
+  });
+})
 
 module.exports = app;

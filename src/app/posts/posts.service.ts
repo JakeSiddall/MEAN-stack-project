9e39da040994904e012159b9552ba2bb99badeb5
Jake Siddall <jake.siddall@gmail.com>
2021-09-14 21:53:12 -0700

    App optimizations and cleanup

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,8 +4,10 @@ import { map } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
 
+import { environment } from "../../environments/environment";
 import { Post } from "./post.model";
 
+const BACKEND_URL = environment.apiUrl + "/posts/";
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
@@ -15,7 +17,7 @@ export class PostsService {
 
   getPosts(postsPerPage: number, currentPage: number) {
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
-    this.http.get<{ message: string, posts: any, maxPosts: number }>('http://localhost:3000/api/posts' + queryParams)
+    this.http.get<{ message: string, posts: any, maxPosts: number }>(BACKEND_URL + queryParams)
     .pipe(map(postData => {
       return { posts: postData.posts.map(post => {
         return {
@@ -49,7 +51,7 @@ export class PostsService {
       content: string,
       imagePath: string,
       creator: string
-    }>("http://localhost:3000/api/posts/" + id);
+    }>(BACKEND_URL + id);
   }
 
   addPost(title: string, content: string, image: File) {
@@ -59,7 +61,7 @@ export class PostsService {
     postData.append("image", image, title);
     this.http
     .post<{ message: string, post: Post }>(
-      'http://localhost:3000/api/posts',
+      BACKEND_URL,
       postData
       )
     .subscribe((responseData) => {
@@ -85,7 +87,7 @@ export class PostsService {
       };
     }
     this.http
-      .put('http://localhost:3000/api/posts/' + id, postData)
+      .put(BACKEND_URL + id, postData)
       .subscribe(response => {
         this.router.navigate(["/"]);
       });
@@ -93,6 +95,6 @@ export class PostsService {
 
   deletePost(postId: string) {
     return this.http
-      .delete('http://localhost:3000/api/posts/' + postId);
+      .delete(BACKEND_URL + postId);
   }
 }
